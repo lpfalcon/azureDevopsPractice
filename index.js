8169cd91956af4f22ca9ed475ef2405cfa8e5c34
lpfalcon <lpfalcon99@gmail.com>
2024-12-16 11:02:01 -0400

    add new feature

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const http = require("http");
 const fs = require('fs');
 
 
+const urlList = {
+    '/': './index.html',
+    '/feature': './feature.html'
+}
 const listener = (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    const html = fs.readFileSync('./index.html');
-    response.end(html);
+
+    if (urlList[request.url]) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        const html = fs.readFileSync(urlList[request.url]);
+        response.end(html);
+    } else {
+        response.writeHead(404);
+        response.write('NOT FOUND');
+        response.end();
+    }
+
 }
 
 const server = http.createServer(listener);
